c60ce321d7357c028f6f81e0d05ae06d61417f57
mKurtycz <michall.kurtycz@gmail.com>
2023-03-22 13:01:26 +0100

    Initial commit

---
+++ src/main/java/priv/mKurtycz/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package priv.mKurtycz.blog.springbootblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+	}
+
+}
