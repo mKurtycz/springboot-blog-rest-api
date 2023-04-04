267c397d06a6af15cec3a4e748546e32fa5c19da
mKurtycz <michall.kurtycz@gmail.com>
2023-04-04 15:28:57 +0200

    Changed the database

--- src/main/java/priv/mKurtycz/blog/springbootblogrestapi/payload/PostDTO.java
+++ src/main/java/priv/mKurtycz/blog/springbootblogrestapi/payload/PostDTO.java
@@ -1,7 +1,6 @@
 package priv.mKurtycz.blog.springbootblogrestapi.payload;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
